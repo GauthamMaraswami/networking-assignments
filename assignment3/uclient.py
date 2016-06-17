d37f5668f91fb6706454d3a46b4c4749d697fc3f
Gdarkstar <gauthamm2015@gmail.com>
2016-06-17 16:23:51 +0530

    Update uclient.py

--- assignment3/uclient.py
+++ assignment3/uclient.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python   
 import socket
-
-# This is an example of a UDP client - it creates
-# a socket and sends data through it
-
-# create the UDP socket
 UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 data = " welcome\n"
-
-# Simply set up a target address and port ...
 addr = ("localhost",21561)
-# ... and send data out to it!
+
 UDPSock.sendto(data,addr)
 data='h'
 
